b0a3f4ae4b41a08a0d742355102e0e5060f50bd4
JackTench <jack.tench00@gmail.com>
2023-06-27 18:49:26 +0100

    Started work on playable game.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Jack Tench 2023
+# This python file just runs a game of yahtzee using this code base.
 
 # Imports
 import player
@@ -7,3 +8,22 @@ import score
 # Global Variables
 title = "pyahtzee"
 version = "0.1a"
+
+# Print statements for top of terminal.
+print(title + " " + version)
+
+# Set amount of player(s).
+playersAmount = int(input("Number of players: "))
+
+# Get player names.
+playerNames = []
+for i in range(0, playersAmount):
+    qString = "Player " + str(i + 1) + "'s Name: "
+    name = input(qString)
+    playerNames.append(name)
+
+# Create player object for each physical player.
+players = []
+for playerName in playerNames:
+    players.append(player.Player(playerName))
+
