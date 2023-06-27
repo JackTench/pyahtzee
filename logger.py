3d9b935e85044bf9053e12f1a52568eee776cbfc
JackTench <jack.tench00@gmail.com>
2023-06-27 17:55:11 +0100

    Hugely borked.

--- logger.py
+++ logger.py
@@ -1,7 +1,22 @@
 # Jack Tench 2023
 
+# Imports
+from datetime import datetime
+
 class Logger():
 
     # Constructor method.
     def __init__(self, name):
-        self.name = name
+
+        # Get current date and time as string when a logger is created.
+        self.dt = datetime.now().strftime("%d.%m.%Y %H:%M")
+
+        # Set logger name.
+        self.name = "Log - " + name + " " + self.dt + ".txt"
+
+        # Create log file.
+        with open(self.name, "w") as self.logfile:
+            self.logfile.write("Logfile created. \n")
+            self.logfile.write(self.name + "\n")
+
+test = Logger("test")
