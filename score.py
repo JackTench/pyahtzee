6608ac3a8d2022bfacb5ca429a494a79cd97b9bf
Jack Tench <jack.tench00@gmail.com>
2023-06-27 14:20:20 +0100

    Huge rewrite of scoring.

--- score.py
+++ score.py
@@ -1,70 +1,34 @@
 # Jack Tench 2023
 # Functions related to score.
 
-# Function to return score for a given set of dice in ones.
+# Helper functions for calculating scores.
+# These functions are based off a gist by markheath on github.
+# https://gist.github.com/markheath/1036722/245773ec132815856fd0e60a37dce0520d57b811
+def count(dice, number):
+    return len([y for y in dice if y == number])
+
+def sumSingle(dice, selected):
+    return sum([x for x in dice if x == selected])
+
+def highestRepeated(dice, minRepeats):
+    unique = set(dice)
+    repeats = [x for x in unique if count(dice, x) >= minRepeats]
+
+def ofAKind(dice, n):
+    return highestRepeated(dice, n) * n
+
+# End of helper functions.
+
+# Functions for Ones, Twos, Threes, Fours, Fives and Sixes.
 def ones(dice):
-    score = 0
-    for die in dice:
-        if die == 1:
-            score += 1
-    return score
-
-# Function to return score for a given set of dice in twos.
+    return sumSingle(dice, 1)
 def twos(dice):
-    score = 0
-    for die in dice:
-        if die == 2:
-            score += 1
-    return score
-
-# Function to return score for a given set of dice in threes.
+    return sumSingle(dice, 2)
 def threes(dice):
-    score = 0
-    for die in dice:
-        if die == 3:
-            score += 1
-    return score
-
-# Function to return score for a given set of dice in fours.
+    return sumSingle(dice, 3)
 def fours(dice):
-    score = 0
-    for die in dice:
-        if die == 4:
-            score += 1
-    return score
-
-# Function to return score for a given set of dice in fives.
+    return sumSingle(dice, 4)
 def fives(dice):
-    score = 0
-    for die in dice:
-        if die == 5:
-            score += 1
-    return score
-
-# Function to return score for a given set of dice in sixes.
+    return sumSingle(dice, 5)
 def sixes(dice):
-    score = 0
-    for die in dice:
-        if die == 6:
-            score += 1
-    return score
-
-# Function to return score for a given set of dice in three of a kind.
-def threeOfAKind(dice):
-
-    for die in dice:
-        if dice.count(die) >= 3:
-            # I seriously have no clue how this works, ask ChatGPT or something.
-            return sum(d for d in dice if d == die)
-        else:
-            return 0
-
-# Function to return score for a given set of dice in four of a kind.
-def fourOfAKind(dice):
-
-    # Yet more stuff I don't understand. It works.
-    for die in dice:
-        if dice.count(die) >= 4:
-            return sum(d for d in dice if d == die)
-        else:
-            return 0
+    return sumSingle(dice, 6)
