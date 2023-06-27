a51f2fea8aa6c8dcd7dcfe90d6b69df9d36bb5d2
Jack Tench <jack.tench00@gmail.com>
2023-06-27 12:43:51 +0100

    Added functions for scoring twos through sixes.

--- score.py
+++ score.py
@@ -1,11 +1,50 @@
 # Jack Tench 2023
 # Functions related to score.
 
-# Functions to return scores for a given set of dice in ones.
+# Function to return score for a given set of dice in ones.
 def ones(dice):
     score = 0
     for die in dice:
-        #print(die)
         if die == 1:
+            score += 1
+    return score
+
+# Function to return score for a given set of dice in twos.
+def twos(dice):
+    score = 0
+    for die in dice:
+        if die == 2:
+            score += 1
+    return score
+
+# Function to return score for a given set of dice in threes.
+def threes(dice):
+    score = 0
+    for die in dice:
+        if die == 3:
+            score += 1
+    return score
+
+# Function to return score for a given set of dice in fours.
+def fours(dice):
+    score = 0
+    for die in dice:
+        if die == 4:
+            score += 1
+    return score
+
+# Function to return score for a given set of dice in fives.
+def fives(dice):
+    score = 0
+    for die in dice:
+        if die == 5:
+            score += 1
+    return score
+
+# Function to return score for a given set of dice in sixes.
+def sixes(dice):
+    score = 0
+    for die in dice:
+        if die == 6:
             score += 1
     return score
